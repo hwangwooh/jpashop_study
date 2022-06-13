f8b9493c360f70cead813922b6e56b642e554ae5
hwh <zaq8077@gmail.com>
2022-06-13 18:15:33 +0900

    Initial commit

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
-import jpabook.jpashop.Category;
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,6 +26,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비지니스 로직//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 
 }
--- src/main/java/jpabook/jpashop/lomboktest.java
+++ src/main/java/jpabook/jpashop/lomboktest.java
@@ -3,9 +3,18 @@ package jpabook.jpashop;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class lomboktest {
+    public String getSrt1() {
+        return srt1;
+    }
+
+    public void setSrt1(String srt1) {
+        this.srt1 = srt1;
+    }
+
     private String srt1;
+
+
 
 }
