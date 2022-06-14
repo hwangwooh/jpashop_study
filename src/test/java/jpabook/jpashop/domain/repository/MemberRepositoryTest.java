361a6b5247c5bb19b668ccef6c7fb6efefe38321
hwh <zaq8077@gmail.com>
2022-06-14 23:07:00 +0900

    Merge remote-tracking branch 'origin/master'

--- src/test/java/jpabook/jpashop/domain/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/domain/repository/MemberRepositoryTest.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop.domain.repository;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.service.MemberService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
